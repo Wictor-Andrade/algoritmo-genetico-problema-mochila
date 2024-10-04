ea0ebf62994deb4d8879870c1add302cb389af6c
Wictor Raphael <wictorrafael@live.com>
2024-10-04 01:10:46 -0300

    feat(main): add multi-execs

--- AlgoritmoGenetico/src/Main.java
+++ AlgoritmoGenetico/src/Main.java
@@ -2,7 +2,11 @@ import utils.Agent;
 
 public class Main {
     public static void main(String[] args) {
-        Agent ag = new Agent(14, 100, 1000, 50);
-        ag.run();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Iteración: " + i);
+            Agent ag = new Agent(14, 100, 1000, 50);
+            ag.run();
+        }
     }
 }
